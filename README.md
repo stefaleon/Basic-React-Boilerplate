de1197d49404517ca348e97aaf3149e328723de7
maimous@gmail.com <stefaleon@gmail.com>
2017-01-02 01:05:00 +0200

    React-router, Foundation, Sass

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# Commit #3
+
+## React Boilerplate
+React-router, Foundation, Sass
+
+
+#### Dependencies
+* Express
+* React
+* ReactDOM
+* react-router
+* axios
+
+#### devDependencies
+
+* webpack
+* babel-core
+* babel-loader
+* babel-preset-es2015
+* babel-preset-react
+
+* css-loader
+* script-loader
+* style-loader
+* jquery
+* foundation-sites
+* sass-loader
+* node-sass
+
+
+&nbsp;
+
+# Commit #1
 ## Basic React Boilerplate
 * webpack -w -> makes bundle.js and monitors for changes
 * node server -> starts server, locally on 3000
--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,24 @@
 const React =  require('react');
 const ReactDOM = require('react-dom');
+const {Route, Router, IndexRoute, hashHistory} = require('react-router');
+const Main = require('Main');
+const About = require('About');
+const Examples = require('Examples');
+
+// Load foundation
+require('style!css!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
+// App CSS
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <h1>React Boilerplate</h1>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+            <Route path="about" component={About} />
+            <Route path="examples" component={Examples} />
+            <IndexRoute component={Main} />
+        </Route>
+    </Router>,
     document.getElementById('app')
 );
